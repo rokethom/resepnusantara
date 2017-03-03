9dc7560f14a9ce4c2955197b2f69a3c965fa7339
Sigit Azis <rokethom@gmail.com>
2017-03-03 16:51:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook/", function (req, res) {
-  if (req.query["hub.verify_token"] === "resep") {
+  if (req.query["hub.verify_token"] === "VERIFICATION_TOKEN") {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
@@ -23,3 +23,57 @@ app.get("/webhook/", function (req, res) {
     res.sendStatus(403);
   }
 });
+
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.FB_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "Saya Robot Resep Nusantara. Sebutkan nama masakan yang akan kamu cari.";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Maaf Data Belum Tersedia untuk masakan ini!"});
+    }
+}
+
