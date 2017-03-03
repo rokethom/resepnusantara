d781fecbad703a977d7fb9407a7eba32e15bcb26
Sigit Azis <rokethom@gmail.com>
2017-03-03 17:15:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 
+var db = mongoose.connect(process.env.MONGODB_URI);
+var Movie = require("./models/movie");
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -25,7 +28,7 @@ app.get("/webhook/", function (req, res) {
 });
 
 // All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
+app.post("/webhook/", function (req, res) {
     // Make sure this is a page subscription
     if (req.body.object == "page") {
         // Iterate over each entry
@@ -75,5 +78,122 @@ function processPostback(event) {
     } else if (payload === "Incorrect") {
         sendMessage(senderId, {text: "Maaf Data Belum Tersedia untuk masakan ini!"});
     }
+}
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
+}
+
+function findMovie(userId, movieTitle) {
+    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url:movieObj.Poster
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, {text: movieObj.Error});
+            }
+        } else {
+            sendMessage(userId, {text: "Something went wrong. Try again."});
+        }
+    });
+}
+
+function getMovieDetail(userId, field) {
+    Movie.findOne({user_id: userId}, function(err, movie) {
+        if(err) {
+            sendMessage(userId, {text: "Something went wrong. Try again"});
+        } else {
+            sendMessage(userId, {text: movie[field]});
+        }
+    });
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
 }
 
